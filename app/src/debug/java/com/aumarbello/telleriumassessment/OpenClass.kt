4edc9409928d7df4819b5e3d2bc07ff224777947
Ahmed Umar <khattabu@gmail.com>
2020-10-26 17:33:52 +0100

    Initial commit

---
+++ app/src/debug/java/com/aumarbello/telleriumassessment/OpenClass.kt
@@ -0,0 +1,8 @@
+package com.aumarbello.telleriumassessment
+
+@Target(AnnotationTarget.CLASS)
+annotation class OpenClass
+
+@OpenClass
+@Target(AnnotationTarget.CLASS)
+annotation class OpenForTesting
